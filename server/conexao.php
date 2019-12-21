b54bb2ef07e5891875ddd3bf3574b09fa5389a89
Henrique Alberto de Andrade <henrique.alberto.andrade@hotmail.com>
2019-12-21 20:49:48 -0200

    Update conexao.php

--- server/conexao.php
+++ server/conexao.php
@@ -7,7 +7,7 @@
 		$db_name = "test";
 		$mysql_user = "root";
         $mysql_pass = "";
-        $server_name = "localhost:3307";
+        $server_name = "localhost:3306";
 		
 		$google_api = '584815719549-hfvfqb3d1hd2601ospa797e0jkmtseaj.apps.googleusercontent.com';
 
