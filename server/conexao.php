c5262e57d6dea22c1766b2c7b41b0e2b2d081b53
IKKRIUS\Henrique <henrique.alberto.andrade@hotmail.com>
2020-02-27 17:21:53 -0300

    Up to date .sql

--- server/conexao.php
+++ server/conexao.php
@@ -3,7 +3,7 @@
 		$db_name = "test";
 		$mysql_user = "root";
         $mysql_pass = "";
-        $server_name = "localhost:3306";
+        $server_name = "localhost:3307";
 
         $conn = mysqli_connect($server_name, $mysql_user, $mysql_pass,$db_name);
 
