1968ebc495d4abb8d31a13f59c84a4117df36e01
IKKRIUS\Henrique <henrique.alberto.andrade@hotmail.com>
2021-11-10 20:31:35 -0300

    Folder created, headerHTML function added

--- server/initialize.php
+++ server/initialize.php
@@ -1,5 +1,7 @@
 <?php
 	$URLDepth = isset($URLDepth) ? $URLDepth : '';
 	require_once $URLDepth."server/connection.php";
+	
 	require_once $URLDepth."server/functions.php";
+	require_once $URLDepth."server/functions/html.php";
 ?>
