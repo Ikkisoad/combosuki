21d4e3632f30f49d8ca2cc513b3754084fabe46f
IKKRIUS\Henrique <henrique.alberto.andrade@hotmail.com>
2021-11-10 01:07:20 -0300

    Update initialize.php

--- server/initialize.php
+++ server/initialize.php
@@ -1,12 +1,5 @@
 <?php
-
 	$URLDepth = isset($URLDepth) ? $URLDepth : '';
 	require_once $URLDepth."server/connection.php";
 	require_once $URLDepth."server/functions.php";
-
-
-	/*
-	$URLDepth = '../';
-	require_once "server/initialize.php";
-	*/
 ?>
