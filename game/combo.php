b5fb37cc04bb6e97a0fa0518f61325af461a8ed8
Guilherme Lopes Pinheiro <guizin.lps@gmail.com>
2022-10-10 16:27:08 -0300

    Update combo.php

--- game/combo.php
+++ game/combo.php
@@ -95,7 +95,7 @@
 
 								<?php
 								copyLinktoclipboard(get_combolink($id_combo));
-								copyCombotoclipboard(get_combo($combo));
+								copyCombotoclipboard($combo);
 							echo '</th>';
 						echo '</tr>';
 						echo '<tr>';
